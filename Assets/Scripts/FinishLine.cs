1a996156c06f9830a3b8875dca1c65fb1e195dc0
Bryan Pritchett <dasassygoat@coderbox.co>
2024-06-26 17:23:50 -0500

    start of finishline script

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("You Win!");
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
