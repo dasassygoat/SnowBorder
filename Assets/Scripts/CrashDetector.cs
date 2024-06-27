5c784870f6c6a4bf31b646c4817f1feca02bc9c6
Bryan Pritchett <dasassygoat@coderbox.co>
2024-06-26 20:14:43 -0500

    crash detection

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.tag == "Head")
+        {
+            Debug.Log("You Crashed");
+        }
     }
 }
